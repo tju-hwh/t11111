09c3aca0308a1c61d180cedebfb3476795cb50a4
HuangWenhao <huangwenhao.i@unicloud.com>
2021-07-20 16:36:47 +0800

    dev commit 全局中间件

---
+++ Gs11/t4-Gin/g11-全局中间件.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+func main() {
+
+	r:=gin.Default()
+	r.Use(MiddleWare())
+	{
+		r.GET("/ce", func(c *gin.Context) {
+			value, _ := c.Get("name")
+			fmt.Println("name: ",value)
+			//页面接收
+			c.JSON(200,gin.H{"name":value})
+		})
+	}
+	r.Run()
+}
+
+func MiddleWare() gin.HandlerFunc{
+	return func(g *gin.Context) {
+		t:=time.Now()
+		fmt.Println("中间件开始执行了")
+		// 设置变量到Context的key中，可以通过Get()取
+		g.Set("name","李")
+		status:=g.Writer.Status()
+		fmt.Println("中间件执行完毕", status)
+		t2 := time.Since(t)
+		fmt.Println("time:", t2)
+	}
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 t11111
 # test
 test 720
+# second dev commit
